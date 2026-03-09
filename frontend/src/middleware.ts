73dc9749a992d8748df9fd18f92d185d45802720
Ruslan Nikolaev <nikolaevra@gmail.com>
2026-03-09 18:19:42 -0400

    changes

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -14,7 +14,7 @@ export async function middleware(request: NextRequest) {
         },
         setAll(cookiesToSet) {
           cookiesToSet.forEach(({ name, value, options }) =>
-            request.cookies.set(name, value, options)
+            request.cookies.set({ name, value, ...options })
           );
           supabaseResponse = NextResponse.next({ request });
           cookiesToSet.forEach(({ name, value, options }) =>
